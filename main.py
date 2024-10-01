ab1656bf6364302eeba48d042bcb093c5c1e0364
enoka silipa <eslip96@gmail.com>
2024-10-01 09:39:08 -0600

    added more assignments

--- main.py
+++ main.py
@@ -347,19 +347,106 @@
 # print(result)
 
 
+# def is_leap_year(year):
+#     if year < 1752:
+#         raise ValueError("Please provide a year after 1752.")
+#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#         print(True)
+#     else:
+#         print(False)
+
+
+# year = int(input("Enter a year to see if its a leap year: "))
+
+
+# is_leap_year(year)
+
+
+# def print_month_calendar(month, year, start_day_of_week, days_in_month):
+
+#     print(f"{' ' * 6}{month} {year}\n")
+
+#     days_of_the_week = (' ' * 2) + "S" + (' ' * 2) + "M" + (' ' * 2) + "T" + (' ' * 2) + "W" + (' ' * 2) + "T" + (' ' * 2) + "F" + (' ' * 2) + "S\n"
+#     print(days_of_the_week)
+
+#     print(" " * (start_day_of_week * 5), end='')
+
+#     for x in range(1, days_in_month + 1):
+#         print(f"{x:3}", end='')
+
+#         if (x + start_day_of_week) % 7 == 0:
+#             print("\n")
+
+#     print("\n")
+
+
+# print_month_calendar("OCT", 1996, 0, 31)
+
+
+# print("* * * ATM MACHINE * * *")
+
+# user_balance = 0
+
+
+# def check_balance():
+#     print('*' * 8)
+#     print(f'{user_balance:.2f}')
+#     print('*' * 8)
+
+
+# def deposit_balance():
+#     global user_balance
+#     deposit_amount = float(input("How much would you like to deposit?:"))
+
+#     if deposit_amount < 0:
+#         print("ERROR: AMOUNT MUST BE GREATER THAN 0")
+#     else:
+#         user_balance += deposit_amount
+#         return print(f'Deposited amount ${deposit_amount:.2f}\nNew balance ${user_balance:.2f}')
+
+
+# def withdraw_amount():
+#     global user_balance
+
+#     withdraw_amount = float(input("How much would you like to withdraw?:"))
+
+#     if withdraw_amount < 0:
+#         print(f"ERROR: AMOUNT MUST BE GREATER THAN 0")
+#     elif withdraw_amount > user_balance:
+#         print(f"ERROR: AMOUNT MUST BE MORE THAN CURRENT BALANCE OF ${user_balance:.2f}")
+#     else:
+#         user_balance -= withdraw_amount
+#         print(f'Amount withdrawed ${withdraw_amount:.2f}.\nCurrent Balance ${user_balance:.2f}')
+
+
+# while True:
+#     user_input = input("\nWhat would you like to do?  \nCheck (B)alance, \n(D)eposit, \n(W)ithdraw \n(Q)uit: ").upper()
+
+#     if user_input == 'B':
+#         check_balance()
+#     elif user_input == 'D':
+#         deposit_balance()
+#     elif user_input == 'W':
+#         withdraw_amount()
+#     elif user_input == 'Q':
+#         print('Thank You come again!')
+#         break
+#     else:
+#         print()
+#         print("incorrect option. Please try again.")
+#         print()
+
+
+# DYNAMIC CALENDER
+
+
 def is_leap_year(year):
     if year < 1752:
         raise ValueError("Please provide a year after 1752.")
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(True)
+        return True
     else:
-        print(False)
-
-
-year = int(input("Enter a year to see if its a leap year: "))
-
-
-is_leap_year(year)
+        return False
 
 
 def print_month_calendar(month, year, start_day_of_week, days_in_month):
@@ -380,4 +467,58 @@ def print_month_calendar(month, year, start_day_of_week, days_in_month):
     print("\n")
 
 
-print_month_calendar("OCT", 1996, 0, 31)
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+
+def get_starting_day_of_week(month, year):
+    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+    if month < 3:
+        year -= 1
+    return (year + int(year / 4) - int(year / 100) + int(year / 400) +
+            t[month - 1] + 1) % 7
+
+
+def get_days_in_month(month, year):
+    global months
+    days_in_month = {
+        "January": 31,
+        "February": 28,
+        "March": 31,
+        "April": 30,
+        "May": 31,
+        "June": 30,
+        "July": 31,
+        "August": 31,
+        "September": 30,
+        "October": 31,
+        "November": 30,
+        "December": 31
+    }
+
+    if is_leap_year(year):
+        days_in_month["February"] = 29
+
+    return days_in_month[months[month]]
+
+
+while True:
+    month = int(input("What month would you like to pick (1-12)? "))
+    year = int(input("What Year? "))
+
+    days_in_month = get_days_in_month(month, year)
+    start_day_of_week = get_starting_day_of_week(month, year)
+
+    print_month_calendar(month, year, start_day_of_week, days_in_month)
