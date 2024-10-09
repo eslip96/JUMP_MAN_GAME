e8585229daca51ec42945506502b5d50704510fd
enoka silipa <eslip96@gmail.com>
2024-10-08 20:01:18 -0600

    added

---
+++ slice.py
@@ -0,0 +1,17 @@
+
+
+my_string = "Coding in Python is amazing"
+
+
+print(my_string[10:17])
+
+
+my_tuple = (1, 5, 89, 234, 289, 445, 578, 623, 777, 898, 900)
+
+print(my_tuple[4:9])
+
+
+my_sentence = "The quick brown fox jumps over the lazy dog"
+
+
+print(f'"{my_sentence[10:25]}"')
